978e640c2a1fb50f7c29a0b39ece1e526634186a
ze <MrZhouYongJian@163.com>
2018-12-24 20:48:53 +0800

    2

--- App.js
+++ App.js
@@ -1,10 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
@@ -18,6 +12,7 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
   render() {
     return (
       <View style={styles.container}>
